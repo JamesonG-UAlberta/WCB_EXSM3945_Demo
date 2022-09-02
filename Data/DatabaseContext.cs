befd45cc0fb798cab391f9e1848e498dee4f0bd9
James Grieve <jgrieve@ualberta.ca>
2022-09-02 10:31:52 -0600

    Model and context creation with skeleton OnModelCreating;

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,40 @@
+﻿using API_Demo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Demo.Data
+{
+    public partial class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base()
+        {
+
+        }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql("server=localhost;user=root;database=api_demo", ServerVersion.Parse("10.4.24-mariadb"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
