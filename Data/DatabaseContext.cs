770076265e682af25b38097245924bc70815348b
James Grieve <jgrieve@ualberta.ca>
2022-09-02 10:42:24 -0600

    Completed context; Migration;

--- Data/DatabaseContext.cs
+++ Data/DatabaseContext.cs
@@ -5,32 +5,31 @@ namespace API_Demo.Data
 {
     public partial class DatabaseContext : DbContext
     {
-        public DatabaseContext() : base()
-        {
-
-        }
-
-        public DatabaseContext(DbContextOptions<DatabaseContext> options)
-            : base(options)
-        {
-        }
+        public DatabaseContext() : base() { }
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
 
         public virtual DbSet<Customer> Customers { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseMySql("server=localhost;user=root;database=api_demo", ServerVersion.Parse("10.4.24-mariadb"));
-            }
+            if (!optionsBuilder.IsConfigured) optionsBuilder.UseMySql("server=localhost;user=root;database=api_demo", ServerVersion.Parse("10.4.24-mariadb"));
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
+            modelBuilder.UseCollation("utf8mb4_general_ci").HasCharSet("utf8mb4");
 
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.ToTable("customer");
+                entity.Property(e => e.Id).HasColumnType("int(11)").HasColumnName("id").ValueGeneratedOnAdd();
+                entity.Property(e => e.FirstName).HasMaxLength(50).HasColumnName("fname");
+                entity.Property(e => e.LastName).HasMaxLength(50).HasColumnName("lname");
 
+            });
 
             OnModelCreatingPartial(modelBuilder);
         }
---
+++ Migrations/20220902163915_InitialMigration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_Demo.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "customer",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int(11)", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    fname = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false, collation: "utf8mb4_general_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    lname = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false, collation: "utf8mb4_general_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_customer", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .Annotation("Relational:Collation", "utf8mb4_general_ci");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "customer");
+        }
+    }
+}
