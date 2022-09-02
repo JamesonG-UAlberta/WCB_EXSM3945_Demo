042a1f3ce6eb9a5fc1ae7ba90b27d1915ef4e5ce
James Grieve <jgrieve@ualberta.ca>
2022-09-02 10:54:27 -0600

    Fixed service exception (API doesn't have DbContext by default);

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using API_Demo.Data;
+
 namespace API_Demo
 {
     public class Program
@@ -12,6 +14,7 @@ namespace API_Demo
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<DatabaseContext>();
 
             var app = builder.Build();
 
