50725bba9f9b15fe85c1ac3e9196ad6463716973
James Grieve <jgrieve@ualberta.ca>
2022-09-02 11:11:13 -0600

    Filled out other methods;

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -49,29 +49,134 @@ namespace API_Demo.Controllers
 
         // POST api/<CustomerController>
         [HttpPost]
-        public void Post(string fname, string lname)
+        public ActionResult Post(string fname, string lname)
         {
-
+            if (string.IsNullOrWhiteSpace(fname) || string.IsNullOrWhiteSpace(lname))
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _context.Customers.Add(new Customer() { FirstName = fname, LastName = lname });
+                _context.SaveChanges();
+                return Ok();
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
         }
 
         // PUT api/<CustomerController>/5
         [HttpPut("{id}")]
-        public void Put(int id, string fname, string lname)
+        public ActionResult Put(string id, string fname, string lname)
         {
-
+            int providedID;
+            Customer found;
+            try
+            {
+                providedID = int.Parse(id);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+            try
+            {
+                found = _context.Customers.Where(x => x.Id == providedID).Single();
+            }
+            catch
+            {
+                return NotFound();
+            }
+            try
+            {
+                found.FirstName = fname??found.FirstName;
+                found.LastName = lname??found.LastName;
+                _context.SaveChanges();
+                return Ok();
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
         }
 
         [HttpPatch("{id}")]
-        public void Patch(int id, string prop, string value)
+        public ActionResult Patch(string id, string prop, string value)
         {
-
+            int providedID;
+            Customer found;
+            try
+            {
+                providedID = int.Parse(id);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+            try
+            {
+                found = _context.Customers.Where(x => x.Id == providedID).Single();
+            }
+            catch
+            {
+                return NotFound();
+            }
+            try
+            {
+                switch (prop)
+                {
+                    case "fname":
+                        found.FirstName = value;
+                        break;
+                    case "lname":
+                        found.LastName = value;
+                        break;
+                    default:
+                        return BadRequest();
+                }
+                _context.SaveChanges();
+                return Ok();
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
         }
 
         // DELETE api/<CustomerController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult Delete(string id)
         {
-
+            int providedID;
+            Customer found;
+            try
+            {
+                providedID = int.Parse(id);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+            try
+            {
+                found = _context.Customers.Where(x => x.Id == providedID).Single();
+            }
+            catch
+            {
+                return NotFound();
+            }
+            try
+            {
+                _context.Customers.Remove(found);
+                _context.SaveChanges();
+                return Ok();
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
         }
     }
 }
